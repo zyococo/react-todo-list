9861961cca5463311cd55492cee42b31d88da83f
g1348032 <g1348032@icloud.com>
2023-04-25 23:59:10 +0900

    checked the coding

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// index >> App >> TodoList >> Todo
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
