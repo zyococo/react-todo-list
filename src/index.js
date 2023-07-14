9a8511ac4cea43771bcff08ec4385d418af61fa3
g1348032 <g1348032@icloud.com>
2023-07-14 14:17:29 +0900

    Brush up

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* コンポーネントのバグを検知したり、予期せぬ処理などを警告してくれるツール */}
     <App />
   </React.StrictMode>
 );
